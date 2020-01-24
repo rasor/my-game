c0b0833302ddcb45f3d534a6a209fca8f1d8cf95
Søren If Raarup <soren.raarup@if.dk>
2020-01-24 12:07:51 +0100

    Calling ionic service

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,17 +1,53 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonAvatar, IonContent, IonHeader, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar, useIonViewWillEnter } from '@ionic/react';
+import React, { useState } from 'react';
 
-const Tab3Page: React.FC = () => {
+interface Person {
+  name: string;
+  email: string;
+  position: string;
+  photo: string;
+}
+
+const Tab3: React.FC = () => {
+
+  const [people, setPeople] = useState<Person[]>([]);
+
+  useIonViewWillEnter(async () => {
+    const result = await fetch('https://uifaces.co/api?limit=25', {
+      headers: { 'x-API-KEY': '873771d7760b846d51d025ac5804ab' }
+    });
+    const data = await result.json();
+    setPeople(data);
+  });
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab Three</IonTitle>
+          <IonTitle>Employees</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent></IonContent>
+      <IonContent>
+        <IonList>
+          {people.map((person, idx) => <EmployeeItem key={idx} person={person} />)}
+        </IonList>
+      </IonContent>
     </IonPage>
   );
 };
 
-export default Tab3Page;
+const EmployeeItem: React.FC<{ person: Person }> = ({ person }) => {
+  return (
+    <IonItem >
+      <IonAvatar slot="start">
+        <img src={person.photo} />
+      </IonAvatar>
+      <IonLabel>
+        <h2>{person.name}</h2>
+        <p>{person.position}</p>
+      </IonLabel>
+    </IonItem>
+  );
+}
+
+export default Tab3;
