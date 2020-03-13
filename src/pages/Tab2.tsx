2d3ef8bf3f274b612b96778cb34b2c91a5df97c7
Søren If Raarup <soren.raarup@if.dk>
2020-03-13 12:30:48 +0100

    p86 tsx

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -19,7 +19,13 @@ const Tab2: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <Game />
+      <Game 
+        gridSize={5}
+        //challengeSize={6}
+        //challengeSeconds={3}
+        //playSeconds={10}
+        //maxWrongAttempts={3}
+      />      
       </IonContent>
     </IonPage>
   );
@@ -27,8 +33,10 @@ const Tab2: React.FC = () => {
 
 export default Tab2;
 
-const Cell: React.FC = () => {
-  return <div className="cell" style={{ width: "33.3%" }} />;
+const Cell: React.FC<{width: number;}> = ({ width }) => {
+  return (
+    <div className="cell" style={{ width: `${width}%` }} />
+  );
 };
 
 const Footer: React.FC = () => {
@@ -42,23 +50,60 @@ const Footer: React.FC = () => {
   );
 };
 
-const Game: React.FC = () => {
+const Game: React.FC<{gridSize: number;}> = ({ gridSize }) => {
+  const cellIds = utils.createArray(gridSize * gridSize);
+  const cellWidth = 100 / gridSize;
   return (
     <div className="game">
       <div className="grid">
-        <Cell />
-        <Cell />
-        <Cell />
-
-        <Cell />
-        <Cell />
-        <Cell />
-
-        <Cell />
-        <Cell />
-        <Cell />
+      {cellIds.map(cellId =>
+        <Cell
+          key={cellId}
+          width={cellWidth}
+        />
+      )}
       </div>
       <Footer />
     </div>
   );
+};
+
+/** https://jscomplete.com/playground/rs3.1 */
+// Math science
+const utils = {
+  /* Create an array based on a numeric size property.
+     Example: createArray(5) => [0, 1, 2, 3, 4] */
+  createArray: (size: number) => Array.from({ length: size }, (_, i) => i),
+
+  /* Pick random elements from origArray up to sampleSize
+     And use them to form a new array.
+     Example: sampleArray([9, 12, 4, 7, 5], 3) => [12, 7, 5] */
+  sampleArray: (origArray: number[], sampleSize: number) => {
+    const copy = origArray.slice(0);
+    const sample = [];
+    for (let i = 0; i < sampleSize && i < copy.length; i++) {
+      const index = Math.floor(Math.random() * copy.length);
+      sample.push(copy.splice(index, 1)[0]);
+    }
+    return sample;
+  },
+
+  /* Given a srcArray and a crossArray, count how many elements 
+     in srcArray exist or do not exist in crossArray.
+     Returns an array like [includeCount, excludeCount]
+     Example: arrayCrossCounts([0, 1, 2, 3, 4], [1, 3, 5]) => [2, 3] */
+  arrayCrossCounts: (srcArray: number[], crossArray: number[]) => {
+    let includeCount = 0;
+    let excludeCount = 0;
+    srcLoop: for (let s = 0; s < srcArray.length; s++) {
+      for (let c = 0; c < crossArray.length; c++) {
+        if (crossArray[c] === srcArray[s]) {
+          includeCount += 1;
+          continue srcLoop;
+        }
+      }
+      excludeCount += 1;
+    }
+    return [includeCount, excludeCount];
+  },
 };
