322348de5a83cb4a96ec44ad30569842b4b36b55
Søren If Raarup <soren.raarup@if.dk>
2020-03-13 11:48:28 +0100

    p84

--- src/pages/Tab1.jsx
+++ src/pages/Tab1.jsx
@@ -18,7 +18,12 @@ const Tab1 = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <Game/>
+        <Game
+          gridSize={5}
+          challengeSize={6}
+          challengeSeconds={3}
+          playSeconds={10}
+          maxWrongAttempts={3}/>
       </IonContent>
     </IonPage>
   );
@@ -43,23 +48,54 @@ const Footer = () => {
   )
 }
 
-const Game = () => {
+const Game = ({ gridSize }) => {
+  const cellIds = utils.createArray(gridSize * gridSize)
   return (
     <div className="game">
       <div className="grid">
-        <Cell/>
-        <Cell/>
-        <Cell/>
-
-        <Cell/>
-        <Cell/>
-        <Cell/>
-
-        <Cell/>
-        <Cell/>
-        <Cell/>
+        {cellIds.map(cellId => <cell key={cellId}/>)}
       </div>
       <Footer/>
     </div>
   )
 }
+
+/** https://jscomplete.com/playground/rs3.1 */
+// Math science
+const utils = {
+  /* Create an array based on a numeric size property.
+     Example: createArray(5) => [0, 1, 2, 3, 4] */
+  createArray: (size) => Array.from({ length: size }, (_, i) => i),
+
+  /* Pick random elements from origArray up to sampleSize
+     And use them to form a new array.
+     Example: sampleArray([9, 12, 4, 7, 5], 3) => [12, 7, 5] */
+  sampleArray: (origArray, sampleSize) => {
+    const copy = origArray.slice(0);
+    const sample = [];
+    for (let i = 0; i < sampleSize && i < copy.length; i++) {
+      const index = Math.floor(Math.random() * copy.length);
+      sample.push(copy.splice(index, 1)[0]);
+    }
+    return sample;
+  },
+
+  /* Given a srcArray and a crossArray, count how many elements 
+     in srcArray exist or do not exist in crossArray.
+     Returns an array like [includeCount, excludeCount]
+     Example: arrayCrossCounts([0, 1, 2, 3, 4], [1, 3, 5]) => [2, 3] */
+  arrayCrossCounts: (srcArray, crossArray) => {
+    let includeCount = 0;
+    let excludeCount = 0;
+    srcLoop: for (let s = 0; s < srcArray.length; s++) {
+      for (let c = 0; c < crossArray.length; c++) {
+        if (crossArray[c] === srcArray[s]) {
+          includeCount += 1;
+          continue srcLoop;
+        }
+      }
+      excludeCount += 1;
+    }
+    return [includeCount, excludeCount];
+  },
+};
